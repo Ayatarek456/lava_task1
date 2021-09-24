8e2c4c87d5b0566078fd36f7a696755b9ddc3b9a
Ayatarek456 <36480818+Ayatarek456@users.noreply.github.com>
2021-09-24 19:06:16 +0200

    Add files via upload

---
+++ task_lava.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 18:48:53 2021
+
+@author: CompuLand
+"""
+
+""
+import hashlib
+import glob
+read_files = glob.glob("*.yml")
+
+with open("result.yml", "wb") as outfile:
+    for f in read_files:
+        with open(f, "rb") as infile:
+            outfile.write(infile.read())
+  #this file finalllllllllllllll to eliminate ant duplication          
+output_file_path=open(r"C:\Users\CompuLand\Downloads\translations\translations\fina.yml","w") 
+#output_file_path="C:\\Users\CompuLand\Downloads\translations\translations\result.yml") 
+#input_file_path ="C:\\Users\CompuLand\Downloads\translations\translations\all_yml.yml")
+completed_lines_hash = set()
+#input_file=input_file_path
+#output_file = open(output_file_path, "w") 
+for line in open(r"C:\Users\CompuLand\Downloads\translations\translations\result.yml","r"):
+  #5
+  if (str(' http_code')) in line:
+        output_file_path.writelines(line)
+        continue
+
+    
+  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+  #6
+  if hashValue not in completed_lines_hash:
+    output_file_path.write(line)
+    completed_lines_hash.add(hashValue)
+#7
+output_file_path.close()
